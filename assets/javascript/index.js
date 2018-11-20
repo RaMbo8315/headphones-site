1b04966de6be2f2c0bddbff63204f09fc2c04829
RaMbo8315 <henryurena@yahoo.com>
2018-11-20 04:27:34 -0500

    added side nav

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     console.log($(document).width());
     var width = $(window).innerWidth();
-    if (width <= 1000) {
+    if (width <= 1028) {
         $(".main-menu").css("display", "none")
         $(".mobile-menu").css("display", "block");
     }
@@ -9,13 +9,16 @@ $(document).ready(function () {
 
     $(window).resize(function () {
         var width = $(window).innerWidth();
-        if (width <= 1003) {
+        if (width <= 1028) {
             $(".main-menu").css("display", "none");
             $(".mobile-menu").css("display", "block");
-        } else if (width >= 1003) {
+        } else if (width >= 1028) {
             $(".main-menu").css("display", "block");
             $(".mobile-menu").css("display", "none");
             $(".flip-container .flipper, .flip-container.hover .flipper").css("transform", "rotateX(0deg)")
+        }
+        if(width >= 629) {
+            $(".sideNav").css("width", "0");
         }
     })
 });
